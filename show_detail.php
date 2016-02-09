9858fc64ec0929a2f84865f99fd7924a3873e155
Pipatpol Tanavongchinda <pipatpol.t@ku.th>
2016-02-09 22:25:22 +0700

    add show detail

--- show_detail.php
+++ show_detail.php
@@ -4,6 +4,8 @@
     $pass = "1234";
     $dbname = "board";
 
+    $item = null;
+    if(isset($_GET["item"])) $item = $_GET["item"];
     try {
         $conn = new PDO("mysql:host=$host;dbname=$dbname",$user,$pass);
         $conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
@@ -13,15 +15,81 @@
 
         $sql = "select *
                 from Question
-                where qno=$_GET[\"item\"]";
-        $dbarr = $conn->query($sql);
+                where qno = $item";
+        $dbarr = $conn->prepare($sql);
+        $dbarr->execute();
+        $qrow = $dbarr->fetch();
     } catch (PDOException $e) {
         echo $sql."<br/>".$e->getMessage();
     }
     $conn = null;
 ?>
 
-Question <b>
+Question
+<b>
 <?php
-
+    echo renHTML($qrow["qtopic"]);
 ?>
+</b><br>
+
+<table width="100%" border="1" bgcolor="#e0e0e0" border="black">
+    <tr>
+        <td>
+            <?php
+                echo renHTML($qrow["qdetail"]);
+            ?><br/>
+            by <b>
+                <?php
+                    echo renHTML($qrow["qname"]);
+                ?>
+            </b>
+        </td>
+    </tr>
+</table><br/>
+<?php
+    try{
+        $conn = new PDO("mysql:host=$host;dbname=$dbname",$user,$pass);
+        $conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+        $sql = "select *
+                from Answer
+                where aquestionno = $item";
+        $count = $conn->prepare($sql);
+        $count->execute();
+        if($count->rowCount() > 0) {
+            foreach ($conn->query($sql) as $row) {
+?>
+Answer No.<b>
+<?php
+                echo $row["ano"];
+?>
+</b><br/>
+    <table width="100%" border="1">
+        <tr>
+            <td>
+<?php
+                echo renHTML($row["adetail"]);
+?><br/>
+by <b>
+<?php
+                echo renHTML($row["aname"]);
+?>
+                </b>
+            </td>
+        </tr>
+<?php
+            }
+        }
+    }
+    catch(PDOException $e){
+        echo $sql."<br/>".$e->getMessage();
+    }
+
+    echo "<form method=post action=add_answer.php?answerno=$item>";
+    $conn = null;
+?>
+Answer: <br/>
+<textarea name="a_answer" rows="5" cols="40"></textarea><br/>
+Name: <input type="text" name="a_name" size="30"><br/><br/>
+<input type="submit" value="Send">&nbsp;
+<input type="reset" value="Cancel">
+</form>
